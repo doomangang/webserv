bb3795681b43fcc590f7c422e673b640846befcd
noweaD <dwm9510@naver.com>
2025-06-08 11:42:41 +0900

    feat: logger.cpp 추가 fix: logger.hpp 기존 로직 L_STATE 제거 fix: webserv.hpp epoll 비활성화

--- inc/Logger.hpp
+++ inc/Logger.hpp
@@ -3,30 +3,27 @@
 
 # include "Webserv.hpp"
 
-/* Not used currently */
+#define RESET          "\x1B[0m"
+#define RED            "\x1B[31m"
+#define LIGHT_RED      "\x1B[91m"
+#define WHITE          "\x1B[37m"
+#define BLINK           "\x1b[5m"
+#define YELLOW         "\x1B[33m"
+#define LIGHT_BLUE     "\x1B[94m"
+#define CYAN           "\x1B[36m"
+#define DARK_GREY      "\x1B[90m"
+#define LIGHTMAGENTA   "\x1B[95m"
+
+
 enum LogPrio{
     DEBUG,
     INFO,
     ERROR
 };
 
-enum L_State{
-    ON,
-    OFF
-};
-
-
 enum Mode{
     CONSOLE_OUTPUT,
     FILE_OUTPUT
-};
-
-enum ErrorType{
-    SYSCALL_ERROR,
-    LOG_ERROR,
-    PARSE_ERROR,
-    CGI_ERROR,
-    SERVER_ERROR
 };
 
 class Logger{
@@ -35,17 +32,12 @@ class Logger{
         static std::string file_name;
         static LogPrio prio;
         static std::map<LogPrio, std::string> prio_str;
-        static L_State state;
 
         static void         setFilenName(std::string);
-        static void         setState(L_State);
-
-        static void         logMsg(const char *, Mode, const char*, ...);
+        static void         logMsg(LogPrio p, Mode, const char*, ...);
         static void         setPrio(LogPrio);
         static void         enableFileLog();
         static std::string  getCurrTime();
-    private:
-        static std::map<LogPrio, std::string> initMap();
 
 };
 
